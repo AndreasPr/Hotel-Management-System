56540e35518e12ffa458b49a5df704cb25f91d9e
Andreas Priftis <andreaspriftisdeveloper@gmail.com>
2024-03-22 23:47:23 -0500

    created newsletter, gallery, search components

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,9 +1,15 @@
+import Gallery from "@/components/Gallery/Gallery";
 import HeroSection from "@/components/HeroSection/HeroSection";
+import NewsLetter from "@/components/NewsLetter/NewsLetter";
+import PageSearch from "@/components/PageSearch/PageSearch";
 
 const Home = () => {
   return (
   <>
     <HeroSection />
+    <PageSearch />
+    <Gallery />
+    <NewsLetter />
   </>
   );
 };
--- src/components/HeroSection/ClientComponent.tsx
+++ src/components/HeroSection/ClientComponent.tsx
@@ -18,15 +18,15 @@ const ClientComponent:FC<Props> = (props) => {
             <div className="flex justify-between mt-12">
                 <div className="flex gap-3 flex-col items-center justify-center">
                     <p className="text-xs lg:text-xl text-center">Basic Room</p>
-                    <CountUpNumber duration={3000} endValue={200}/>
+                    <CountUpNumber duration={3000} endValue={150}/>
                 </div>
                 <div className="flex gap-3 flex-col items-center justify-center">
                     <p className="text-xs lg:text-xl text-center">Luxury Room</p>
-                    <CountUpNumber duration={3000} endValue={200}/>
+                    <CountUpNumber duration={3000} endValue={30}/>
                 </div>
                 <div className="flex gap-3 flex-col items-center justify-center">
                     <p className="text-xs lg:text-xl text-center">Suite</p>
-                    <CountUpNumber duration={3000} endValue={200}/>
+                    <CountUpNumber duration={3000} endValue={50}/>
                 </div>
             </div>
         </div>
